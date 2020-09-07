b091966bfc7c4645bdc51eac0372ff64bc7996f2
Vidit Shah <vidit0210@gmail.com>
2020-09-07 21:24:57 +0530

    pairs

---
+++ LinearAlgebra/Vectors.py
@@ -0,0 +1,14 @@
+from typing import List
+
+Vector = List[float]
+height_weight_age = [70, 170, 40]
+grades = [95, 80, 75, 62]
+
+
+def add(v: Vector, w: Vector) -> Vector:
+    """Adds corresponding element"""
+    assert len(v) == len(w)
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+
+assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
