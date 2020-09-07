b165afa460efd36c05a6291c12b77289a28859da
Vidit Shah <vidit0210@gmail.com>
2020-09-07 22:49:15 +0530

    subtract

--- LinearAlgebra/Vectors.py
+++ LinearAlgebra/Vectors.py
@@ -12,3 +12,14 @@ def add(v: Vector, w: Vector) -> Vector:
 
 
 assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
+
+
+def subtract(v: Vector, w: Vector) -> Vector:
+    """Subtracts corresponding elements"""
+    assert len(v) == len(w)
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+
+assert subtract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
+
+def
