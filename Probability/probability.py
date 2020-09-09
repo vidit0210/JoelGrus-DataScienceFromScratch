188af217ff39872085bec9c7a722033cca80f352
Vidit Shah <vidit0210@gmail.com>
2020-09-09 18:47:37 +0530

    prob

--- Probability/probability.py
+++ Probability/probability.py
@@ -56,3 +56,13 @@ def inverse_normal_cdf(p: float, mu: float = 0, sigma: float = 0, tolerance: flo
     """ Find Approximate inverse using binary search """
     if mu != 0 or sigma != 1:
         return mu + sigma + inverse_normal_cdf(p, tolerance=tolerance)
+    low_z = -10.0
+    hi_z = 10.0
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2
+        mid_p = normal_cdf(mid_z)
+        if mid_p < p:
+            low_z = mid_z
+        else:
+            hi_z = mid_z
+    return mid_z
