c5e5e581df16b66e8fef6c210236757de5fba971
Vidit Shah <vidit0210@gmail.com>
2020-09-10 00:54:52 +0530

    gd

---
+++ GradientDescent/GradientDescent.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from typing import Callable
+from LinearAlgebra import Vector, dot
+
+
+def sum_of_squares(v: Vector) -> float:
+    """Computes the sum of squared elements in v"""
+    return dot(v, v)
+
+
+def difference_quotient(f: Callable[[float], float], x: float, h: float) -> float:
+    return (f(x+h) - f(x))/h
+
+
+def square(x: float) -> float:
+    return x * x
+
+
+def derivative(x: float) -> float:
+    return 2 * x
+
+
+xs = range(-10, 11)
+actuals = [derivative(x) for x in xs]
+estimated = [difference_quotient(square, x, h=0.001) for x in xs]
+
+plt.title("Actual Derivate cs Estimate")
+plt.plot(xs, actuals, 'rx', label='Actual')
+plt.plot(xs, estimates, 'b+', label='Estimate')
+plt.legend(loc=9)
+plt.show()
