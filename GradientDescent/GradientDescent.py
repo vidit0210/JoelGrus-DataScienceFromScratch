1005e7a5489d1882f91ef8ad12bb792c68abb0fd
Vidit Shah <vidit0210@gmail.com>
2020-09-10 00:59:09 +0530

    estimates

--- GradientDescent/GradientDescent.py
+++ GradientDescent/GradientDescent.py
@@ -22,10 +22,16 @@ def derivative(x: float) -> float:
 
 xs = range(-10, 11)
 actuals = [derivative(x) for x in xs]
-estimated = [difference_quotient(square, x, h=0.001) for x in xs]
+estimates = [difference_quotient(square, x, h=0.001) for x in xs]
 
 plt.title("Actual Derivate cs Estimate")
 plt.plot(xs, actuals, 'rx', label='Actual')
 plt.plot(xs, estimates, 'b+', label='Estimate')
 plt.legend(loc=9)
 plt.show()
+
+
+def partial_difference_quotient(f: Callable[[Vector], float], v: Vector, i: int, h: float) -> float:
+    """Returns the -th partial difference quotient of at v"""
+    w = [v_j + (h if j == i else 0) for j, v_j in enumerate(v)]
+    return (f(w)-f(v))/h
