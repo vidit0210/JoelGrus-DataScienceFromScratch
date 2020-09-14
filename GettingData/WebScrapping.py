462a8c9471e6a6ab094ee23012d3718df65559e3
Vidit Shah <vidit0210@gmail.com>
2020-09-14 18:33:07 +0530

    bs4

--- GettingData/WebScrapping.py
+++ GettingData/WebScrapping.py
@@ -1,4 +1,12 @@
-
+from bs4 import BeautifulSoup
+import requests
 
 URL = ("https://raw.githubusercontent.com/"
        "joelgrus/data/master/getting-data.html")
+html = requests.get(URL).text
+soup = BeautifulSoup(html, 'html5lib')
+
+first_paragraph = soup.find('p')
+# print(first_paragraph)
+first_paragraph_text = soup.p.text
+print(first_paragraph_text)
