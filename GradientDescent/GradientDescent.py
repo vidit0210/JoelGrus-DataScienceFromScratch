115c169f5ad4f788bdc258f96ff10b4b0c7acf2f
Vidit Shah <vidit0210@gmail.com>
2020-09-14 14:24:52 +0530

    assertion

--- GradientDescent/GradientDescent.py
+++ GradientDescent/GradientDescent.py
@@ -1,3 +1,5 @@
+from LinearAlgebra import distance, add, scalar_multiply
+import random
 import matplotlib.pyplot as plt
 from typing import Callable
 from LinearAlgebra import Vector, dot
@@ -29,3 +31,36 @@ plt.plot(xs, actuals, 'rx', label='Actual')
 plt.plot(xs, estimates, 'b+', label="Estimate")
 plt.legend(loc=9)
 plt.show()
+
+
+def partial_difference_quotient(f: Callable[[Vector], float], v: Vector, i: int, h: float) -> float:
+    """Returns the ith partial difference quotient of f at v"""
+    w = [v_j + (h if j == i else 0) for j, v_j in enumerate(v)]
+    return (f(w)-f(v))/h
+
+
+def estimate_gradient(f: Callable[[Vector], float], v: Vector, h: float = 0.0001):
+    return [partial_difference_quotient(f, v, i, h) for i in range(len(v))]
+
+
+def gradient_step(v: Vector, gradient: Vector, step_size: float) -> Vector:
+    """Moves step size in the gradient direction from v"""
+    assert len(v) == len(gradient)
+    step = scalar_multiply(step_size, gradient)
+    return add(v, step)
+
+
+def sum_of_squares_gradient(v: Vector) -> Vector:
+    return [2 * v_i for v_i in v]
+
+
+# Pick a random starting point
+v = [random.uniform(-10, 10) for i in range(3)]
+
+for epoch in range(1000):
+    grad = sum_of_squares_gradient(v)
+    v = gradient_step(v, grad, -0.01)
+    print(epoch, v)
+
+# V should be close to 0
+assert distance(v, [0, 0, 0]) < 0.001, "Distance Assertion"
