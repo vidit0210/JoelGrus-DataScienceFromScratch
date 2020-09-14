2c8a55db190f099273a78c2036f254329f65b140
Vidit Shah <vidit0210@gmail.com>
2020-09-14 16:44:19 +0530

    data

---
+++ GettingData/files.py
@@ -0,0 +1,14 @@
+with open('email_addresses.txt', 'w') as f:
+    f.write("joelgrus@gmail.com\n")
+    f.write("joel@m.datasciencester.com\n")
+    f.write("joelgrus@m.datasciencester.com\n")
+
+
+def get_domain(email_address: str) -> str:
+    """Split on '@' and return the last piece"""
+    return email_address.lower().split("@")[-1]
+
+
+# Ac ouple of tests
+assert get_domain('joelgrus@gmail.com') == 'gmail.com'
+assert get_domain('joel@m.datasciencester.com') == 'm.datasciencester.com'
