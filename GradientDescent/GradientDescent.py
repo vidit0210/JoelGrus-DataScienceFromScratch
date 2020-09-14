e9c63629fd99f1ca17bcd844045115e1615005c8
Vidit Shah <vidit0210@gmail.com>
2020-09-14 13:59:49 +0530

    derivative

--- GradientDescent/GradientDescent.py
+++ GradientDescent/GradientDescent.py
@@ -1,1 +1,19 @@
+from typing import Callable
 from LinearAlgebra import Vector, dot
+
+
+def sum_of_squares(v: Vector) -> float:
+    """Computes the sum of squared elements in v"""
+    return dot(v, v)
+
+
+def difference_quotient(f: Callable[[float], [float]], x: float, h: float) -> float:
+    return (f(x+h) - f(x))/h
+
+
+def square(x: float) -> float:
+    return x*x
+
+
+def derivative(x: float) -> float:
+    return 2*x
