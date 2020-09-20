61155197c736669c353425dda00c9da70b6efa7b
Vidit Shah <vidit0210@gmail.com>
2020-09-20 17:07:42 +0530

    s2

--- Introduction/SoicialNetwork.py
+++ Introduction/SoicialNetwork.py
@@ -16,32 +16,3 @@ friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
 
 friendships = {user["id"]: [] for user in users}
 print(friendships)
-
-# And loop over the friendsip pairs to populate it
-for i, j in friendship_pairs:
-    friendships[i].append(j)
-    friendships[j].append(i)
-
-
-def number_of_friends(user):
-    """How many friends does this user have """
-    user_id = user["id"]
-    friends_ids = friendships[user_id]
-    return len(friends_ids)
-
-
-total_connections = sum(number_of_friends(user) for user in users)
-
-num_users = len(users)
-avg_connections = total_connections / num_users
-
-num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
-
-num_friends_by_id.sort(
-    key=lambda id_and_friends: id_and_friends[1], reverse=True)
-
-
-def foaf_ids_bad(user):
-    """foaf is short for "friend of a friends " """
-    return [foaf_id for friend_id in friendships[user["id"]]
-            for foaf_id in friendships[friend_id]]
