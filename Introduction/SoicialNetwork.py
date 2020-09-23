457fe3c0c52ad1f64cda1a837d77ba1ba327a0c2
Vidit Shah <vidit0210@gmail.com>
2020-09-23 11:05:20 +0530

    f1

--- Introduction/SoicialNetwork.py
+++ Introduction/SoicialNetwork.py
@@ -14,9 +14,22 @@ users = [
 friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
                     (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
+# initialize the dict iwth an empty list for each user id
 friendships = {user["id"]: [] for user in users}
 print(friendships)
+
+# And Loop over the fiendship pair to populate it
 for i, j in friendship_pairs:
     friendships[i].append(j)
     friendships[j].append(i)
 print(friendships)
+
+
+def number_of_friends(user):
+    """How many friends does the user have """
+    user_id = user["id"]
+    friends_id = friendships[user_id]
+    return len(friends_id)
+
+
+assert number_of_friends(users[0]) == 2
